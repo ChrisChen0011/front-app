b720fcb3223f75ee3610b69d0893b24ad9af348f
max <kingcall550102@gmail.com>
2024-06-05 23:19:41 +0800

    hello world from home mac

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Item from './pages/home/components/Item';
 import List from './pages/home/components/List';
 import './index.css';
 
+
+// hello world
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
